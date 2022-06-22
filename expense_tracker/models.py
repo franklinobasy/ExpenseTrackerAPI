cef2e7388e317dca3aa6d9c6d8a173211e942105
Franklin Obasi <franklinobasy@gmail.com>
2022-06-22 16:22:59 +0100

    modified models.py

--- expense_tracker/models.py
+++ expense_tracker/models.py
@@ -9,9 +9,15 @@ class User(db.Model):
         public_id = db.Column(db.String(50))
         expenses = db.relationship('Expense', backref='user')
 
+        def __repr__(self):
+                return f"User <{self.username}>-{self.id}"
+
 class Expense(db.Model):
         id = db.Column(db.Integer, primary_key= True)
         description = db.Column(db.Text)
         amount = db.Column(db.Float)
         date = db.Column(db.DateTime, default=datetime.utcnow)
         user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+        def __repr__(self):
+                return f"Expense <{self.description}-{self.id}, User <{self.user_id}>"
